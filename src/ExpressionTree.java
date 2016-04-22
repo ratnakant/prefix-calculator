8804800d530a7b0b1bbe8733896ab08ebc7a8ab7
Ratnakant Sardal <ratnakant@yahoo.com>
2016-04-21 21:00:08 -0700

    Added project config

---
+++ src/ExpressionTree.java
@@ -0,0 +1,100 @@
+
+
+import java.util.Scanner;
+
+public class ExpressionTree{
+
+	private static class TreeNode{
+		private final boolean leaf; // ?Is this a leaf? else internal
+		private final char op; // For an internal node, the operator
+		private double value; // For a leaf, the value
+		private TreeNode left, // Left subexpression (internal node)
+		right; // Right subexpression
+		
+		// Constructor
+		private TreeNode (boolean leaf, char op, double value){
+			this.leaf = leaf;
+			this.op = op;
+			this.value = value;
+			this.left = null; // Empty to start
+			this.right = null;
+		}
+		
+		// For leaf nodes, show the value; for internal, the operator.
+		public String toString(){
+			return leaf ? Double.toString(value) : Character.toString(op); 
+		}
+	} 
+
+	TreeNode root = null;
+	
+	public ExpressionTree (Scanner input){
+		root = build(input);
+	}
+
+	/**
+	 * Based on a white-space delimited prefix expression, build the
+	 * corresponding binary expression tree.
+	 * @param input The scanner with the expression
+	 * @return reference to the corresponding binary expression tree
+	 */
+	private TreeNode build (Scanner input){
+		boolean leaf;
+		String token;
+		double value;
+		TreeNode node;
+
+		leaf = input.hasNextDouble();
+		if (leaf){
+			value = input.nextDouble();
+			node = new TreeNode (leaf, '\0', value);
+		}else{
+			token = input.next();
+
+			node = new TreeNode (leaf, token.charAt(0), 0.0);
+			node.left = build (input);
+			node.right = build (input);
+		}
+		return node;
+	}
+
+	/**
+	 * Evaluate the expression and return its value.
+	 * All the work is done in the private recursive method.
+	 * @return the value of the expression tree.
+	 */
+	public double evaluate (){
+		return root == null ? 0.0 : evaluate (root);
+	}
+	
+	// Evaluate the expression: for internal nodes, this amounts
+	// to a post-order traversal, in which the processing is doing
+	// the actual arithmetic. For leaf nodes, it is simply the
+	// value of the node.
+	private double evaluate (TreeNode node){
+		double result; // Value to be returned
+		if (node.leaf) // Just get the value of the leaf
+			result = node.value;
+		else{
+			// Evaluating the expression
+			double left, right;
+			char operator = node.op;
+			// Capture the values of the left and right subexpressions
+			left = evaluate (node.left);
+			right = evaluate (node.right);
+			
+			// Do the arithmetic, based on the operator
+			switch ( operator ){
+				case '-': result = left - right; break;
+				case '*': result = left * right; break;
+				case '/': result = left / right; break;
+				// NOTE: allow fall-through from default to case '+'
+				default: System.out.println ("Unrecognized operator " +
+					operator + " treated as +.");
+				case '+': result = left + right; break;
+			}
+		}
+		// Return either the leaf's value or the one we just calculated.
+		return result;
+	}
+}
---
+++ src/PrefixCalc.java
@@ -0,0 +1,19 @@
+
+
+import java.util.Scanner;
+
+public class PrefixCalc{
+	
+	public static void main ( String[] args ){
+		System.out.print("Enter Prefix Expression: ");
+		Scanner sc = new Scanner(System.in);
+		String inString = sc.nextLine();
+		Calculator myCalculator = new PrefixCalculatorImpl();
+		
+		Number result = myCalculator.calculate(inString);
+
+		System.out.println ("\nResult: " + result.doubleValue());
+
+		sc.close();
+	}
+} 
